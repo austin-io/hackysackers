06275e7a9b9c0aca052165b4c5f8574b6cd1405a
roborhin0 <apoberezhnik23@gmail.com>
2018-11-18 02:05:16 -0800

    Animation Sprites

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+onready var ANI = get_node("./AnimatedSprite")
+
 var motion = Vector2()
 var speed = 400
 var dx = 1
@@ -10,25 +12,39 @@ func _ready():
 
 func _process(delta):
 	
+	var animation = "idle"
+	
 	if(Input.is_action_pressed("ui_select")):
-		dx = 3
-		dy = 3
+		dx = 2
+		dy = 2
 	else:
 		dx = 1
 		dy = 1
 	
 	if(Input.is_action_pressed("ui_right")):
 		motion.x = speed * dx
+		ANI.flip_h = false
+		animation = "walking"
+		
 	elif(Input.is_action_pressed("ui_left")):
 		motion.x = -speed * dx
+		ANI.flip_h = true
+		animation = "walking"
+		
 	else:
 		motion.x = 0 * dx
 		
 	if(Input.is_action_pressed("ui_up")):
 		motion.y = -speed * dy
+		animation = "walking"
+		
 	elif(Input.is_action_pressed("ui_down")):
 		motion.y = speed * dy
+		animation = "walking"
+		
 	else:
 		motion.y = 0 * dy
-		
+	
+	ANI.play(animation)
+	
 	move_and_slide(motion)
